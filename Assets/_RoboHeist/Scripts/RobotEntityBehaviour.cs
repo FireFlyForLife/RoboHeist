08fff9773b45c29ddd1aedc79130eda75da5eefa
FireFlyForLife <maiko.s.99@gmail.com>
2025-08-02 16:38:29 +0200

    Made clicking on robots works

--- Assets/_RoboHeist/Scripts/RobotEntityBehaviour.cs
+++ Assets/_RoboHeist/Scripts/RobotEntityBehaviour.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[SelectionBase]
 public class RobotEntityBehaviour : MoveableEntityBehaviour
 {
     public RobotEntityData robotEntityData;
