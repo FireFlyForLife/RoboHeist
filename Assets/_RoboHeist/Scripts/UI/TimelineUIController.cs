fcd62efc8ce4519cd532bf3a5fba5a3b90a49d51
FireFlyForLife <maiko.s.99@gmail.com>
2025-08-03 13:47:25 +0200

    Added ability to roll back time

--- Assets/_RoboHeist/Scripts/UI/TimelineUIController.cs
+++ Assets/_RoboHeist/Scripts/UI/TimelineUIController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -11,11 +12,14 @@ public class TimelineUIController : MonoBehaviour
 	private SliderInt timeSlider;
 	private ToggleButtonGroup playButtonGroup;
 	private Label pauseNotification;
+	private bool isDraggingTimeSlider = false;
 
-    private float theTime;
+    public static float TheTime;
+    public const float TickDelay = 1.0f;
+	private float maxTime;
 
-	// Start is called once before the first execution of Update after the MonoBehaviour is created
-	void Start()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
 	{
 		levelBuilder = FindAnyObjectByType<LevelBuilder>();
 
@@ -24,19 +28,81 @@ public class TimelineUIController : MonoBehaviour
 		playButtonGroup = ui.Q<ToggleButtonGroup>("PlayButtonGroup");
 		pauseNotification = ui.Q<Label>("PauseNotification");
 
-
         timeSlider.RegisterValueChangedCallback(OnTimeValueChanged);
+		timeSlider.RegisterCallback<MouseCaptureEvent>(OnTimeValueStartedDragging);
+		timeSlider.RegisterCallback<MouseCaptureOutEvent>(OnTimeValueStoppedDragging);
 		playButtonGroup.RegisterValueChangedCallback(OnPlayStateChanged);
 		playButtonGroup.value = new ToggleButtonGroupState(0b100, 3);
 
-        theTime = Time.time;
+        TheTime = Time.time;
 	}
 
-	private void OnTimeValueChanged(ChangeEvent<int> e)
+    private void OnTimeValueStartedDragging(MouseCaptureEvent e)
+    {
+		isDraggingTimeSlider = true;
+		Debug.Log("Started time dragging");
+		Time.timeScale = 0.0f;
+    }
+
+    private void OnTimeValueStoppedDragging(MouseCaptureOutEvent e)
+    {
+		isDraggingTimeSlider = false;
+        Debug.Log("Stopped time dragging");
+
+        RobotEntityBehaviour[] robots = RobotEntityBehaviour.allRobots.ToArray(); //FindObjectsByType<RobotEntityBehaviour>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        foreach (var robot in robots)
+        {
+            // Find the last entry we want to keep
+            // This will never be -1, since the initial entry we add is set with time=-1, meaning it is always there
+            int index = robot.transformHistory.FindLastIndex((historicalTransform) => historicalTransform.time <= TheTime);
+
+            // Set position back
+            robot.robotEntityData.position = robot.transformHistory[index].pos;
+            robot.robotEntityData.direction = robot.transformHistory[index].direction;
+            robot.instructionQueue.SetInstructions(robot.transformHistory[index].instructionQueueState.GetAllInstructions().ToArray());
+			robot.instructionQueue.SetInstructionPointer(robot.transformHistory[index].instructionQueueState.GetInstructionPointer());
+
+            // Clear rolled back entries
+            if (!isDraggingTimeSlider)
+            {
+                maxTime = TheTime;
+                robot.transformHistory.RemoveRange(index + 1, robot.transformHistory.Count - (index + 1));
+            }
+        }
+
+		Time.timeScale = 1.0f;
+    }
+
+    private void OnTimeValueChanged(ChangeEvent<int> e)
 	{
-		// TODO: actually rewind
-		timeSlider.SetValueWithoutNotify(e.previousValue);
-	}
+		// Update time value
+        TheTime = Mathf.Min(e.newValue * TickDelay, maxTime);
+        timeSlider.SetValueWithoutNotify(Mathf.FloorToInt(TheTime / TickDelay)); // In case we clamped the time, reflect that clamp in the UI slider pos
+
+        Debug.Log("Time value dragged!");
+
+        // Rewind robot positions
+        RobotEntityBehaviour[] robots = RobotEntityBehaviour.allRobots.ToArray(); //FindObjectsByType<RobotEntityBehaviour>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+		foreach (var robot in robots)
+		{
+			// Find the last entry we want to keep
+			// This will never be -1, since the initial entry we add is set with time=-1, meaning it is always there
+			int index = robot.transformHistory.FindLastIndex((historicalTransform) => historicalTransform.time <= TheTime);
+			
+			// Set position back
+			robot.robotEntityData.position = robot.transformHistory[index].pos;
+			robot.robotEntityData.direction = robot.transformHistory[index].direction;
+            robot.instructionQueue.SetInstructions(robot.transformHistory[index].instructionQueueState.GetAllInstructions().ToArray());
+            robot.instructionQueue.SetInstructionPointer(robot.transformHistory[index].instructionQueueState.GetInstructionPointer());
+
+            // Clear rolled back entries
+            if (!isDraggingTimeSlider)
+			{
+				maxTime = TheTime;
+                robot.transformHistory.RemoveRange(index + 1, robot.transformHistory.Count - (index + 1));
+			}
+        }
+    }
 
 	private void OnPlayStateChanged(ChangeEvent<ToggleButtonGroupState> e)
 	{
@@ -56,11 +122,11 @@ public class TimelineUIController : MonoBehaviour
 	void Update()
 	{
 		// Update time value
-		theTime += Time.deltaTime;
+		TheTime += Time.deltaTime;
+		maxTime = Mathf.Max(maxTime, TheTime);
 
-		// Update slider UI
-		const float instructionDelay = 1.0f;
-		int amount_of_ticks = Mathf.FloorToInt(theTime / instructionDelay);
+        // Update slider UI
+        int amount_of_ticks = Mathf.FloorToInt(TheTime / TickDelay);
 		if (amount_of_ticks > timeSlider.highValue)
 			timeSlider.highValue = amount_of_ticks;
 		timeSlider.SetValueWithoutNotify(amount_of_ticks);
